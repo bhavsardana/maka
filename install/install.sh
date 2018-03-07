20834c17a837d0575e4417474eaa004f5fd18245
Bhav Sardana <bhavsardana@gmail.com>
2018-03-08 01:42:10 +0530

    test

--- install/install.sh
+++ install/install.sh
@@ -21,3 +21,5 @@ curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.6.3.tgz
 tar -zxvf mongodb-linux-x86_64-3.6.3.tgz
 mkdir -p mongodb
 cp -R -n mongodb-linux-x86_64-3.6.3/ mongodb
+
+#test entry
