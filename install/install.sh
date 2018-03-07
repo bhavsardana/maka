846a813c15b019f5a6473f7f56b531db197ab38e
Sumedh Singh <sumedhsingh12@gmail.com>
2018-03-07 23:40:53 +0530

    Update install.sh
    Adding steps for EC2 setup

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,15 @@
 #main script for installation of the tool and its dependencies
 #for now writing steps for mongodb installation here
 
+#adding steps for setup of EC2 instances:
+sudo yum update -y
+yum install httpd -y
+sudo yum install git
+service httpd start
+chkconfig http on
+git clone https://github.com/bhavsardana/maka.git
+sudo yum install -y mongodb-org
+
 #installation using apt-get
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
